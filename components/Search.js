935b3ae2e613ecb4518cbc1785804dcc553dc95c
Shams Benhamou <shamsbenhamou@gmail.com>
2024-03-05 15:58:14 +0100

    feat: add search

--- components/Search.js
+++ components/Search.js
@@ -1,22 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { View, FlatList, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text, Pressable, Keyboard } from 'react-native';
 import axios from 'axios';
+import Card from './Card';
 
-// Components
-
-const Search = () => {
-
+const Search = ({ navigation }) => {
     const styles = StyleSheet.create({
         container: {
-            margin: 0,
-            width: '100%',
-            flex: 1,
+            margin: 20,
+        },
+        input: {
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            marginBottom: 10,
+            paddingHorizontal: 10,
+        },
+        button: {
+            height: 40,
+            backgroundColor: '#252525', // Couleur de fond du bouton
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 5,
+            color: 'white',
+            display: 'flex',
+            textAlign: 'center',
+        },
+        buttonText: {
+            color: 'white',
         },
     });
 
+    const [search, setSearch] = useState('');
+    const [resultId, setResultId] = useState(null);
+
+
+    const searchPokemon = async () => {
+        Keyboard.dismiss();
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`);
+            setResultId(response.data.id);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Recherche</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Entrez votre recherche..."
+                onChangeText={(text) => setSearch(text)} // Fonction de gestion du changement de texte
+            />
+            <Pressable onPress={searchPokemon} style={styles.button}>
+                <Text style={styles.buttonText}>RECHERCHER</Text>
+            </Pressable>
+            {resultId && <Card id={resultId} navigation={navigation} />}
         </View>
     );
 };
